4a9afec9a4da06f8ad7d628cd626d502e639abfc
Aubrey Nations <aubreynations@yahoo.com>
2014-10-27 15:22:34 -0500

    100pt done

--- 70-100pt.py
+++ 70-100pt.py
@@ -17,8 +17,7 @@ grass = drawpad.create_rectangle(0,550,805,605, fill = "green")
 housebody = drawpad.create_rectangle(250,300,550,550, fill = "red")
 
 #Making the roof of the house
-leftroof = drawpad.create_line(250,300,400,200)
-rightroof = drawpad.create_line(400,200,550,300)
+roof = drawpad.create_polygon(250,300,400,200,550,300, fill = "pink", outline = "black")
 
 #Making the door of the house
 door = drawpad.create_rectangle(350,400,450,550, fill = "pink")
@@ -28,11 +27,13 @@ windows = drawpad.create_rectangle(275,400,325,350, fill = "lightblue")
 windows2 = drawpad.create_rectangle(475,400,525,350, fill = "lightblue")
 
 #Making the door knob for the house
-doorknob = drawpad.create_oval(425,500,435,490, fill = "yellow")
+doorknob = drawpad.create_oval(425,500,435,490, fill = "yellow", outline = "red")
 
 #Making the chimney for the house
-leftchimney = drawpad.create_line(480,225,480,255)
-rightchimney = drawpad.create_line(545,225,545,297)
+chimney = drawpad.create_polygon(480,225,480,255,545,225,545,297, fill = "pink", outline = "black")
 topchimney = drawpad.create_rectangle(475,200,550,225, fill = "red")
+
+#An awesome sun! So inspiring. So beautiful. Yes.
+sun = drawpad.create_oval(50,50,150,150, fill = "yellow")
 
 root.mainloop()
